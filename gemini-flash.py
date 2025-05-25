1fcbb05b3b6f0b106c8918a6bd028bee781b7dc7
Vitor Nery <vitormanoel2020@gmail.com>
2025-05-24 22:59:54 -0300

    Adicionado implementação do Gemini e atualizações de credenciais

---
+++ gemini-flash.py
@@ -0,0 +1,9 @@
+from config import GEMINI_API_KEY
+from google import genai
+
+client = genai.Client(api_key=GEMINI_API_KEY)
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash", contents="Cite uma frase de nelson mandela"
+)
+print(response.text)
